150d7a64ef2e441335a009da3395ab5ad225bce7
Peter Strøiman <peter@stroiman.com>
2023-01-21 10:37:17 +0100

    Fix gitignore and add cool stuff

--- lua/stroiman/packer.lua
+++ lua/stroiman/packer.lua
@@ -1,5 +1,15 @@
-print("Hello packer")
 require('packer').startup(function(use)
-	  use 'wbthomason/packer.nvim'
-	  
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use 'rose-pine/neovim'
+	use { "ellisonleao/gruvbox.nvim", config = function()
+		vim.cmd('colorscheme gruvbox')
+	end}
+
+	use('nvim-tresitter/nvim-treesitter', { run = ':TSUpdate'})
+
 end)
