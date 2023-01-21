794d4bff36ce8b0c4e231af2b96a3466923e3f96
Peter Strøiman <peter@stroiman.com>
2023-01-21 11:50:23 +0100

    Add fun keymaps

--- lua/stroiman/remap.lua
+++ lua/stroiman/remap.lua
@@ -3,3 +3,15 @@ vim.keymap.set("n", "-", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "<C-s>", ":w<cr>")
 vim.keymap.set("i", "<C-s>", "<esc>:w<cr>")
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Don't overwrite register when pasting in visual mode
+vim.keymap.set('x', "p", "\"_dP")
