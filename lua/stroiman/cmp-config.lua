ae69c09dd7254eedfe4ff1a18d20c2959f464364
Peter Strøiman <peter@stroiman.com>
2023-01-23 16:54:55 +0100

    Move stuff around

--- lua/stroiman/cmp-config.lua
+++ lua/stroiman/cmp-config.lua
@@ -1,13 +1,25 @@
 local lsp = require('lsp-zero')
 local cmp = require('cmp')
 
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+-- local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<tab>'] = cmp.mapping.confirm({ select = true }),
-  ['<CR>'] = cmp.config.disable,
-  ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i', 'c' }),
+    ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 'c' }),
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-o>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+
+  -- ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  -- ['<C-n>'] = cmp.mapping.select_prev_item(cmp_select),
+  -- ['<tab>'] = cmp.mapping.confirm({ select = true }),
+  -- ['<CR>'] = cmp.config.disable,
+  -- ['<C-Space>'] = cmp.mapping.complete(),
 })
 
 cmp.setup({
--- lua/stroiman/lsp-config.lua
+++ lua/stroiman/lsp-config.lua
@@ -18,6 +18,9 @@ lsp.on_attach(function(_, bufnr)
   end
 
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  -- nmap('K', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 end)
 
 lsp.nvim_workspace()
