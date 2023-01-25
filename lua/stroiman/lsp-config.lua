a6fe241ee7231bc329b06bf7f814cd58831d2415
Peter Strøiman <peter@stroiman.com>
2023-01-25 08:53:06 +0100

    Experiment with LSP setup

--- lua/stroiman/lsp-config.lua
+++ lua/stroiman/lsp-config.lua
@@ -9,6 +9,19 @@ lsp.ensure_installed({
 })
 
 lsp.on_attach(function(_, bufnr)
+    -- vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
+    vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
+    vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
+    vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
+    vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+    vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+    vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
+    vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+    vim.cmd("command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()")
+    vim.cmd("command! LspDiagNext lua vim.lsp.diagnostic.goto_next()")
+    vim.cmd("command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()")
+    vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")    
+
   local nmap = function(keys, func, desc)
     if desc then
       desc = 'LSP: ' .. desc
@@ -17,6 +30,16 @@ lsp.on_attach(function(_, bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
+  -- buf_map(bufnr, "n", "gd", ":LspDef<CR>")
+  nmap("gy", ":LspTypeDef<CR>")
+  nmap("K", ":LspHover<CR>")
+  nmap("[a", ":LspDiagPrev<CR>")
+  nmap("]a", ":LspDiagNext<CR>")
+  nmap("ga", ":LspCodeAction<CR>")
+  nmap("<Leader>a", ":LspDiagLine<CR>")
+  nmap("<C-x><C-x>", "<cmd> LspSignatureHelp<CR>")
+
+  nmap("<leader>rr", ":LspRename<CR>, '[R]efactor [R]ename")
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   -- nmap('K', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
