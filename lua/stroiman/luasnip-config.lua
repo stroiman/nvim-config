b277cc6db4f30ce15a9252d460972d2d5a324387
Peter Strøiman <peter@stroiman.com>
2023-01-25 08:52:35 +0100

    Change snippet config

--- lua/stroiman/luasnip-config.lua
+++ lua/stroiman/luasnip-config.lua
@@ -1,13 +1,36 @@
-local luasnip = require("luasnip")
+local ls = require("luasnip")
+local types = require "luasnip.util.types"
 
-require("luasnip.loaders.from_vscode").lazy_load({ paths = '/Users/peter/.config/nvim/my_snippets' })
--- require("luasnip.loaders.from_snipmate").load({ paths = '/Users/peter/.config/nvim/my_snippets' })
--- require("luasnip.loaders.from_vscode").load()
+-- require("ls.loaders.from_snipmate").load({ paths = '/Users/peter/.config/nvim/my_snippets' })
+-- require("ls.loaders.from_vscode").load()
 
-vim.cmd('command! LuaSnipEdit :lua require("luasnip.loaders").edit_snippet_files()')
+-- require("luasnip.loaders.from_vscode").lazy_load({ paths = '/Users/peter/.config/nvim/my_snippets' })
+-- vim.cmd('command! LuaSnipEdit :lua require("ls.loaders").edit_snippet_files()')
 
-vim.keymap.set('i', '<C-j>', function() luasnip.jump(1) end )
-vim.keymap.set('i', '<C-h>', function() luasnip.jump(-1) end )
+ls.config.set_config {
+    history = true,
+    updateevents = "TextChanged,TextChangedI",
+    enable_autosnippets = true,
+    ext_opts = {
+        [types.choiceNode] = {
+            active = {
+                virt_text = { { "<-", "Error" } }
+            }
+        }
+    }
+}
+
+vim.keymap.set({'i', 's'}, '<C-k>', function()
+    if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+    end
+end, { silent = true })
+
+vim.keymap.set({'i', 's'}, '<C-j>', function()
+    if ls.jumpable(-1) then
+        ls.jump(-1)
+    end
+end, { silent = true })
 
 -- vim.keymap.set("i", "<C-n>", "<Plug>luasnip-next-choice", {})
 -- vim.keymap.set("s", "<C-n>", "<Plug>luasnip-next-choice", {})
