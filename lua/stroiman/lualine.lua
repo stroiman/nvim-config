1d74aea5b524cabbc370bef4466c1caab49d2298
Peter Strøiman <peter@stroiman.com>
2024-01-19 12:01:09 +0100

    Move lua files to lua dir

--- lua/stroiman/lualine.lua
+++ lua/stroiman/lualine.lua
@@ -4,5 +4,13 @@ require("lualine").setup {
     icons_enabled = true,
     -- component_separators = '|',
     -- section_separators = '',
+  },
+  sections = {
+    lualine_a = {
+      {
+        'filename',
+        path = 1
+      }
+    }
   }
 }
