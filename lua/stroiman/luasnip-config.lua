0709ce98e0f20e5988e3b8d9ef4e00689a05bfe2
Peter Strøiman <peter@stroiman.com>
2024-01-20 10:04:24 +0100

    Basic cmp configuration

--- lua/stroiman/luasnip-config.lua
+++ lua/stroiman/luasnip-config.lua
@@ -1,41 +1,40 @@
-local ok, ls = pcall(require,"luasnip")
+local ok, ls = pcall(require, "luasnip")
 if ok then
-local types = require "luasnip.util.types"
+  local types = require("luasnip.util.types")
 
--- require("ls.loaders.from_snipmate").load({ paths = '/Users/peter/.config/nvim/my_snippets' })
--- require("ls.loaders.from_vscode").load()
+  -- require("ls.loaders.from_snipmate").load({ paths = '/Users/peter/.config/nvim/my_snippets' })
+  -- require("ls.loaders.from_vscode").load()
 
-require("luasnip.loaders.from_vscode").lazy_load({ paths = '/Users/peter/.config/nvim/my_snippets' })
-vim.cmd('command! LuaSnipEdit :lua require("ls.loaders").edit_snippet_files()')
+  require("luasnip.loaders.from_vscode").lazy_load({ paths = "/Users/peter/.config/nvim/my_snippets" })
+  vim.cmd('command! LuaSnipEdit :lua require("ls.loaders").edit_snippet_files()')
 
-ls.config.set_config {
+  ls.config.set_config({
     history = true,
     updateevents = "TextChanged,TextChangedI",
     enable_autosnippets = true,
     ext_opts = {
-        [types.choiceNode] = {
-            active = {
-                virt_text = { { "<-", "Error" } }
-            }
-        }
-    }
-}
+      [types.choiceNode] = {
+        active = {
+          virt_text = { { "<-", "Error" } },
+        },
+      },
+    },
+  })
 
-vim.keymap.set({'i', 's'}, '<C-k>', function()
+  vim.keymap.set({ "i", "s" }, "<C-k>", function()
     if ls.expand_or_jumpable() then
-        ls.expand_or_jump()
+      ls.expand_or_jump()
     end
-end, { silent = true })
+  end, { silent = true })
 
-vim.keymap.set({'i', 's'}, '<C-j>', function()
+  vim.keymap.set({ "i", "s" }, "<C-j>", function()
     if ls.jumpable(-1) then
-        ls.jump(-1)
+      ls.jump(-1)
     end
-end, { silent = true })
+  end, { silent = true })
 
--- vim.keymap.set("i", "<C-n>", "<Plug>luasnip-next-choice", {})
--- vim.keymap.set("s", "<C-n>", "<Plug>luasnip-next-choice", {})
--- vim.keymap.set("i", "<C-p>", "<Plug>luasnip-prev-choice", {})
--- vim.keymap.set("s", "<C-p>", "<Plug>luasnip-prev-choice", {})
-
+  -- vim.keymap.set("i", "<C-n>", "<Plug>luasnip-next-choice", {})
+  -- vim.keymap.set("s", "<C-n>", "<Plug>luasnip-next-choice", {})
+  -- vim.keymap.set("i", "<C-p>", "<Plug>luasnip-prev-choice", {})
+  -- vim.keymap.set("s", "<C-p>", "<Plug>luasnip-prev-choice", {})
 end
--- lua/stroiman/packer.lua
+++ lua/stroiman/packer.lua
@@ -1,10 +1,10 @@
 -- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   is_bootstrap = true
-  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
-  vim.cmd [[packadd packer.nvim]]
+  vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+  vim.cmd([[packadd packer.nvim]])
 end
 
 vim.cmd([[
@@ -14,90 +14,120 @@ vim.cmd([[
   augroup end
 ]])
 
-local status, packer = pcall(require, 'packer')
+local status, packer = pcall(require, "packer")
 if not status then
   return
 end
 
 return packer.startup(function(use)
-  use('tpope/vim-unimpaired')
-  use('christoomey/vim-tmux-navigator')
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+  use("tpope/vim-unimpaired")
+  use("christoomey/vim-tmux-navigator")
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.4",
     -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {'nvim-telescope/telescope-ui-select.nvim' }
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
+  use({ "nvim-telescope/telescope-ui-select.nvim" })
   -- use('nvim-telescope/telescope-project.nvim')
-  use{
-    'ahmedkhalf/project.nvim',
+  use({
+    "ahmedkhalf/project.nvim",
     config = function()
-      require("project_nvim").setup {
-      }
-    end
-  }
-  use('vim-scripts/vimwiki')
-  use('mattn/calendar-vim')
+      require("project_nvim").setup({})
+    end,
+  })
+  use("vim-scripts/vimwiki")
+  use("mattn/calendar-vim")
 
-  use('rose-pine/neovim')
-  use('ellisonleao/gruvbox.nvim')
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use('bluz71/vim-nightfly-guicolors')
+  use("rose-pine/neovim")
+  use("ellisonleao/gruvbox.nvim")
+  use({ "catppuccin/nvim", as = "catppuccin" })
+  use("bluz71/vim-nightfly-guicolors")
 
   use("kyazdani42/nvim-web-devicons")
 
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
   -- use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
+  use("theprimeagen/harpoon")
+  use("mbbill/undotree")
+  use("tpope/vim-fugitive")
 
   -- LSP and completion
-  use {
-    'wbthomason/packer.nvim',
+  use({
+    "wbthomason/packer.nvim",
     config = function()
-      require('mason').setup()
-    end
-  }
-  use {'williamboman/mason.nvim'}
-  use {'williamboman/mason-lspconfig.nvim'}
-  use 'neovim/nvim-lspconfig'
-  use 'nvimtools/none-ls.nvim'
+      require("mason").setup()
+    end,
+  })
+  use({ "williamboman/mason.nvim" })
+  use({ "williamboman/mason-lspconfig.nvim" })
+  use("neovim/nvim-lspconfig")
+  use("nvimtools/none-ls.nvim")
 
   -- Navigation
-  use {
+  use({
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    requires = { 
+    requires = {
       "nvim-lua/plenary.nvim",
       -- "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  }
+    },
+  })
 
   -- Git stuff
-  use('tpope/vim-projectionist')
-  use('tpope/vim-rhubarb')
-  use('lewis6991/gitsigns.nvim')
+  use("tpope/vim-projectionist")
+  use("tpope/vim-rhubarb")
+  use("lewis6991/gitsigns.nvim")
 
-  use('nvim-lualine/lualine.nvim')
-  use {
-    'numToStr/Comment.nvim',
+  use("nvim-lualine/lualine.nvim")
+  use({
+    "numToStr/Comment.nvim",
     config = function()
-      require('Comment').setup()
-    end
-  }
+      require("Comment").setup()
+    end,
+  })
 
   -- use 'mhinz/vim-startify'
 
   -- use 'nkrkv/nvim-treesitter-rescript'
-  use {
-    'kevinhwang91/nvim-ufo',
+  use({
+    "kevinhwang91/nvim-ufo",
     requires = {
-      { 'kevinhwang91/promise-async' }
-    }
-  }
+      { "kevinhwang91/promise-async" },
+    },
+  })
+
+  use({
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "neovim/nvim-lspconfig",
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- " For vsnip users.
+      -- Plug 'hrsh7th/cmp-vsnip'
+      -- Plug 'hrsh7th/vim-vsnip'
+      --
+      -- " For luasnip users.
+      -- " Plug 'L3MON4D3/LuaSnip'
+      -- " Plug 'saadparwaiz1/cmp_luasnip'
+      --
+      -- " For ultisnips users.
+      -- " Plug 'SirVer/ultisnips'
+      -- " Plug 'quangnguyen30192/cmp-nvim-ultisnips'
+      --
+      -- " For snippy users.
+      -- " Plug 'dcampos/nvim-snippy'
+      -- " Plug 'dcampos/cmp-snippy'}
+      -- }
+    },
+  })
 
   if is_bootstrap then
     require("packer").sync()
