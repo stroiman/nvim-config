4d10b4fa546a9de35300bfee693f3f24a9eee0f4
Peter Strøiman <peter@stroiman.com>
2024-06-16 18:50:49 +0200

    Add lazy.nvim and git

--- init.lua
+++ init.lua
@@ -1,12 +1,23 @@
 function Reload() 
+	for name, _ in pairs(package.loaded) do
+		if name:match("^stroiman") then
+			package.loaded[name] = nil
+		end
+	end
+
 	dofile(vim.env.MYVIMRC)
 	vim.notify("Configuration reloaded!", vim.log.levels.INFO)
 end
+
 
 vim.g.mapleader = " "
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("n", "<leader>vs", Reload)
 vim.keymap.set("n", "<leader>ve", ":tabnew +tcd\\ %:p:h $MYVIMRC<cr>")
 vim.keymap.set("n", "<C-s>", ":w<cr>")
+vim.keymap.set("i", "<C-s>", "<esc>:w<cr>")
 
-
+if not vim.g.lazy_loaded then
+	require("stroiman.plugins")
+	vim.g.lazy_loaded = true
+end
