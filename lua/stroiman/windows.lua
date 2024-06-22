9a75ab327dc0e5be8385b1b764b00750b40cf1b0
Peter Strøiman <peter@stroiman.com>
2024-06-22 23:50:28 +0200

    Fix help window

--- lua/stroiman/windows.lua
+++ lua/stroiman/windows.lua
@@ -2,13 +2,11 @@ local win_events = vim.api.nvim_create_augroup("stroiman_win_events", { })
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
   group = win_events,
-  callback = function(ev) 
+  callback = function(ev)
     local filetype = vim.fn.getbufvar(ev.buf, "&filetype")
     if (filetype == "help") then
       vim.cmd [[
-        if winnr('$') > 2
-          wincmd K
-        else
+        if winnr('$') < 3
           wincmd L
         endif
         ]]
