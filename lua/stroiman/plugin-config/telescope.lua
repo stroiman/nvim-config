f4d5d7d0ded57aaaa5afa63ba86c0d5c6121e165
Peter Strøiman <peter@stroiman.com>
2024-06-23 09:34:14 +0200

    Add lsp telescope actions

--- lua/stroiman/plugin-config/telescope.lua
+++ lua/stroiman/plugin-config/telescope.lua
@@ -1,4 +1,4 @@
-function make_builtin(name) 
+function make_builtin(name)
   return function()
     require("telescope.builtin")[name]()
   end
@@ -9,3 +9,5 @@ vim.keymap.set('n', '<leader>fg', make_builtin("live_grep"), {})
 vim.keymap.set('n', '<leader>fb', make_builtin("buffers"), {})
 vim.keymap.set('n', '<leader>fh', make_builtin("help_tags"), {})
 vim.keymap.set('n', '<leader>ft', make_builtin("builtin"), {})
+vim.keymap.set('n', '<leader>fd', make_builtin("lsp_definitions"), {})
+vim.keymap.set('n', '<leader>fr', make_builtin("lsp_references"), {})
