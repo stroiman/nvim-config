673e02cd25b68e77f2ae30341dd4821df9582d25
Peter Strøiman <peter@stroiman.com>
2024-06-23 09:18:11 +0200

    cmp stuff

--- lua/stroiman/plugin-config/nvim-cmp.lua
+++ lua/stroiman/plugin-config/nvim-cmp.lua
@@ -19,7 +19,7 @@ local setup_cmp = function()
       ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
       ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
       ["<C-o>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-      ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ["<C-y>"] = cmp.config.disable,
       ["<ESC>"] = cmp.mapping(cmp.mapping.close(), { "i", "c" }),
       ["<C-e>"] = cmp.mapping({
         i = cmp.mapping.abort(),
@@ -35,35 +35,24 @@ local setup_cmp = function()
     })
   })
 
-  -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
-  -- Set configuration for specific filetype.
-  --[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' },
-    }, {
-      { name = 'buffer' },
-    })
- })
- require("cmp_git").setup() ]] --
-
-  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  })
-
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false }
-  })
+  -- -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  -- cmp.setup.cmdline({ '/', '?' }, {
+  --   mapping = cmp.mapping.preset.cmdline(),
+  --   sources = {
+  --     { name = 'buffer' }
+  --   }
+  -- })
+  --
+  -- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  -- cmp.setup.cmdline(':', {
+  --   mapping = cmp.mapping.preset.cmdline(),
+  --   sources = cmp.config.sources({
+  --     { name = 'path' }
+  --   }, {
+  --     { name = 'cmdline' }
+  --   }),
+  --   matching = { disallow_symbol_nonprefix_matching = false }
+  -- })
   vim.g.stroiman_cmp_loaded = true
 end
 
