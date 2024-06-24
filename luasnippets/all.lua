d7c68892c30ae6d299819bd09adde71c4ec4b447
Peter Strøiman <peter@stroiman.com>
2024-06-24 13:17:52 +0200

    Make a uuid snippet

--- luasnippets/all.lua
+++ luasnippets/all.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+  s({
+    trig = "uuid",
+    desc = "Generate a random UUID. Works by calling system `uuid`"
+  }, f(function()
+    local obj = vim.system({ "uuid" }, { text = true }):wait()
+    local result = vim.fn.trim(obj.stdout)
+    return result
+  end
+  ))
+}
