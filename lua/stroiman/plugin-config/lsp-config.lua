c37e703b36c5a8b28bec188fdcd52c3d433be105
Peter Strøiman <peter@stroiman.com>
2024-06-25 10:25:04 +0200

    Add border around popups

--- lua/stroiman/plugin-config/lsp-config.lua
+++ lua/stroiman/plugin-config/lsp-config.lua
@@ -7,6 +7,11 @@ local setup_lspconfig = function()
   local lspconfig = require("lspconfig");
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   local lsp_default_capabilities = require('cmp_nvim_lsp').default_capabilities()
+  local handlers = {
+    ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+    ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+  }
+
   capabilities = vim.tbl_deep_extend('force', capabilities, lsp_default_capabilities)
   lspconfig.tsserver.setup({
     capabilities = capabilities
