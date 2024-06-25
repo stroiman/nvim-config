8601475f91e9f1a8f13f8231f88e20019a4d6b4f
Peter Strøiman <peter@stroiman.com>
2024-06-25 12:00:49 +0200

    Dynamically load all config files

--- lua/stroiman/plugin-config/init.lua
+++ lua/stroiman/plugin-config/init.lua
@@ -1,9 +1,14 @@
 vim.api.nvim_create_augroup("stroiman_plugin_load", {})
 
-require("stroiman.plugin-config.telescope")
+-- Make sure these are loaded first, so they are available if other
+-- plugins cause errors during loading, these are avaiable for
+-- navigating vim config files
 require("stroiman.plugin-config.projectionist")
-require("stroiman.plugin-config.treesitter")
-require("stroiman.plugin-config.lsp-config")
-require("stroiman.plugin-config.nvim-cmp")
-require("stroiman.plugin-config.none-ls")
-require("stroiman.plugin-config.luasnip")
+require("stroiman.plugin-config.telescope")
+require("stroiman.plugin-config.harpoon")
+
+for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config') .. '/lua/stroiman/plugin-config', [[v:val =~ '\.lua$']])) do
+  if file ~= 'init.lua' then
+    require('stroiman.plugin-config.' .. file:gsub('%.lua$', ''))
+  end
+end
