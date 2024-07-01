7d88d8c6001e8c9296050920f3d017643f3a4ab1
Peter Strøiman <peter@stroiman.com>
2024-07-01 11:24:12 +0200

    Disable sign column for help files

--- lua/stroiman/windows.lua
+++ lua/stroiman/windows.lua
@@ -1,18 +1,26 @@
 local win_events = vim.api.nvim_create_augroup("stroiman_win_events", {})
 
+vim.api.nvim_create_autocmd("FileType", {
+  group = win_events,
+  pattern = "help",
+  callback = function()
+    vim.cmd([[ setlocal signcolumn=no]])
+  end,
+})
+
 vim.api.nvim_create_autocmd("BufWinEnter", {
   group = win_events,
   callback = function(ev)
     local filetype = vim.fn.getbufvar(ev.buf, "&filetype")
-    if (filetype == "help") then
+    if filetype == "help" then
       local winnr = vim.fn.winnr("$")
       local winsize = vim.fn.winwidth(0)
       if winnr < 3 and winsize >= 140 then
-        vim.cmd [[
+        vim.cmd([[
           wincmd L
           vertical resize 80
-      ]]
+      ]])
       end
     end
-  end
+  end,
 })
