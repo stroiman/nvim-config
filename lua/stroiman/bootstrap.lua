06302a7108837405471aa91398e68b40880a0c45
Peter Strøiman <peter@stroiman.com>
2024-07-01 11:32:02 +0200

    Fix bootstrapping issue (only sync current vim)

--- lua/stroiman/bootstrap.lua
+++ lua/stroiman/bootstrap.lua
@@ -1,3 +1,9 @@
+local plugin_dir = "plugins"
+local config_path = vim.fn.stdpath("config")
+local lua_path = config_path .. "/lua"
+local plugin_pattern = lua_path .. "/" .. plugin_dir .. "/*.lua"
+local should_sync_on_reload = false
+
 if not vim.g.stroiman_lazy_loaded then
   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
   if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -12,29 +18,41 @@ if not vim.g.stroiman_lazy_loaded then
   end
   vim.opt.rtp:prepend(lazypath)
 
-  require("lazy").setup("plugins", {
+  require("lazy").setup(plugin_dir, {
     ui = {
-      border = "rounded"
+      border = "rounded",
     },
     change_detection = {
-      notify = false
-    }
+      enabled = true,
+      notify = false,
+    },
   })
   vim.g.stroiman_lazy_loaded = true
 end
 
-local group = vim.api.nvim_create_augroup("stroiman_lazy_events", {});
+local group = vim.api.nvim_create_augroup("stroiman_lazy_events", {})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = group,
+  pattern = plugin_pattern,
+  callback = function()
+    should_sync_on_reload = true
+  end,
+})
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "LazyReload",
   group = group,
   callback = function()
-    vim.cmd("Lazy sync")
-  end
+    if should_sync_on_reload then
+      vim.cmd("Lazy sync")
+      should_sync_on_reload = true
+    end
+  end,
 })
 
 local reload_plugin = function()
-  local path = vim.fn.expand "%:t:r"
+  local path = vim.fn.expand("%:t:r")
   vim.cmd("Lazy reload " .. path)
 end
 
