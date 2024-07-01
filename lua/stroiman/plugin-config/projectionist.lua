2a0776037f1ab38397a08ebfe15e1471b92929da
Peter Strøiman <peter@stroiman.com>
2024-07-01 11:24:45 +0200

    Add typescript projections

--- lua/stroiman/plugin-config/projectionist.lua
+++ lua/stroiman/plugin-config/projectionist.lua
@@ -1,10 +1,20 @@
 vim.g.projectionist_heuristics = {
   ["init.lua"] = {
     ["lua/plugins/*.lua"] = {
-      alternate = "lua/stroiman/plugin-config/{}.lua"
+      alternate = "lua/stroiman/plugin-config/{}.lua",
     },
     ["lua/stroiman/plugin-config/*.lua"] = {
-      alternate = "lua/plugins/{}.lua"
+      alternate = "lua/plugins/{}.lua",
     },
-  }
+  },
+  ["package.json&tsconfig.json"] = {
+    ["src/*.ts"] = {
+      command = "src",
+      alternate = { "test/{}.test.ts" },
+    },
+    ["test/*.test.ts"] = {
+      command = "test",
+      alternate = "src/{}.ts",
+    },
+  },
 }
