4e5d12ba8f82efaaaced378df0f541550276d730
Peter Strøiman <peter@stroiman.com>
2024-07-01 11:28:10 +0200

    More visible line numbers with catppuccin

--- lua/stroiman/plugin-config/catppuccin.lua
+++ lua/stroiman/plugin-config/catppuccin.lua
@@ -1,8 +1,16 @@
 local setup_catppuccin = function()
-  print("FOOBAR")
   local catppuccin = require("catppuccin")
   catppuccin.setup({
     transparent_background = true,
+    custom_highlights = function(colors)
+      return {
+        LineNr = { fg = "#707070" },
+        -- Comment = { fg = colors.flamingo },
+        -- TabLineSel = { bg = colors.pink },
+        -- CmpBorder = { fg = colors.surface2 },
+        -- Pmenu = { bg = colors.none },
+      }
+    end,
   })
 
   vim.cmd.colorscheme("catppuccin")
