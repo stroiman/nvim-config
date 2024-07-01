2c8b0f162d15c7cc73cb0168d920f0153c60c10c
Peter Strøiman <peter@stroiman.com>
2024-07-01 11:26:17 +0200

    Colorscheme experiments

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -2,12 +2,5 @@ return {
   "christoomey/vim-tmux-navigator",
   "folke/tokyonight.nvim",
   "mbbill/undotree",
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      -- vim.cmd.colorscheme "gruvbox"
-    end,
-  },
   "kyazdani42/nvim-web-devicons",
 }
---
+++ lua/stroiman/plugin-config/gruvbox.lua
@@ -0,0 +1,44 @@
+local setup_gruvbox = function()
+  -- Default options:
+  require("gruvbox").setup({
+    -- terminal_colors = true, -- add neovim terminal colors
+    -- undercurl = true,
+    -- underline = true,
+    -- bold = true,
+    -- italic = {
+    --   strings = true,
+    --   emphasis = true,
+    --   comments = true,
+    --   operators = false,
+    --   folds = true,
+    -- },
+    -- strikethrough = true,
+    -- invert_selection = false,
+    -- invert_signs = false,
+    -- invert_tabline = false,
+    -- invert_intend_guides = false,
+    -- inverse = true, -- invert background for search, diffs, statuslines and errors
+    -- contrast = "", -- can be "hard", "soft" or empty string
+    -- palette_overrides = {},
+    -- overrides = {},
+    -- dim_inactive = false,
+    transparent_mode = true,
+  })
+  -- vim.cmd("colorscheme gruvbox")
+
+  vim.g.stroiman_gruvbox_setup = true
+end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyLoad",
+  group = "stroiman_plugin_load",
+  callback = function(ev)
+    if ev.data == "gruvbox" then
+      setup_gruvbox()
+    end
+  end,
+})
+
+if vim.g.stroiman_gruvbox_setup then
+  setup_gruvbox()
+end
