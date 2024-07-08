9542bfc0832b0520b0aef156f6321788158022d4
Peter Strøiman <peter@stroiman.com>
2024-07-08 09:50:05 +0200

    Make gruvgox transparent

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,5 +2,17 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   lazy = false,
+  opts = {
+    transparent_background = true,
+    custom_highlights = function(colors)
+      return {
+        LineNr = { fg = "#707070" },
+        -- Comment = { fg = colors.flamingo },
+        -- TabLineSel = { bg = colors.pink },
+        -- CmpBorder = { fg = colors.surface2 },
+        -- Pmenu = { bg = colors.none },
+      }
+    end,
+  },
   priority = 1000,
 }
