ee5db0aec387cf8f1e63160b675e430c7d30d501
Peter Strøiman <peter@stroiman.com>
2024-07-08 09:48:06 +0200

    Add <C-space> to trigger completion

--- lua/stroiman/plugin-config/nvim-cmp.lua
+++ lua/stroiman/plugin-config/nvim-cmp.lua
@@ -14,6 +14,7 @@ local setup_cmp = function()
     },
     mapping = cmp.mapping.preset.insert({
       -- ["<C-k>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+      ["<C-space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
       ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
       ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
       ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
