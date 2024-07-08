048293c89e1e4f10b7e6a21c6048d0617b845b6e
Peter Strøiman <peter@stroiman.com>
2024-07-08 12:24:24 +0200

    Disable which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,5 +1,5 @@
 return { -- Useful plugin to show you pending keybinds.
-  "folke/which-key.nvim",
-  name = "which-key",
-  event = "VimEnter", -- Sets the loading event to 'VimEnter'
+  -- "folke/which-key.nvim",
+  -- name = "which-key",
+  -- event = "VimEnter", -- Sets the loading event to 'VimEnter'
 }
