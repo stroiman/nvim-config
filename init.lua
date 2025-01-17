9f99e24dc987921fb8fc994a6f9079cc2eea727e
Peter Strøiman <peter@stroiman.com>
2025-01-17 20:30:00 +0100

    Fix netrw issues

--- init.lua
+++ init.lua
@@ -15,6 +15,7 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 vim.g.netrw_banner = 0
 vim.g.netrw_list_hide = [[^\.git\/$]]
+vim.g.netrw_sort_sequence = "\\/$,*"
 vim.o.timeout = true
 vim.o.timeoutlen = 500
 
