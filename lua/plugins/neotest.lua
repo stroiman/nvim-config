1a36c748f8490342dbe6d5191ac3c563a9891b17
Peter Strøiman <peter@stroiman.com>
2025-03-28 16:27:07 +0100

    Disable neotest.
    It was crap

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,16 +1,18 @@
-return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    {
-      "fredrikaverpil/neotest-golang",
-      version = "*",
-      -- dependencies = {
-      --   "andythigpen/nvim-coverage", -- Added dependency
-      -- },
-    },
-  },
-}
+return {}
+-- return {
+--   "nvim-neotest/neotest",
+--   dependencies = {
+--     "nvim-neotest/nvim-nio",
+--     "nvim-lua/plenary.nvim",
+--     "antoinemadec/FixCursorHold.nvim",
+--     "nvim-treesitter/nvim-treesitter",
+--     {
+--       -- "nvim-neotest/neotest-go",
+--       "fredrikaverpil/neotest-golang",
+--       version = "*",
+--       -- dependencies = {
+--       --   "andythigpen/nvim-coverage", -- Added dependency
+--       -- },
+--     },
+--   },
+-- }
