158124b277857dcc892b3c1c43efe4ebe740f4cf
Peter Strøiman <peter@stroiman.com>
2025-04-05 08:44:18 +0200

    Add nordic keymap

---
+++ lua/plugins/nordic.lua
@@ -0,0 +1,12 @@
+return {
+  "AlexvZyl/nordic.nvim",
+  name = "nordic",
+  lazy = false,
+  priority = 1000,
+  -- config = function()
+  --   -- require("nordic").load()
+  --   require("nordic").setup({
+  --     bold_keywords = true,
+  --   })
+  -- end,
+}
---
+++ lua/stroiman/plugin-config/nordic.lua
@@ -0,0 +1,28 @@
+local setup_nordic = function()
+  require("nordic").load()
+  require("nordic").setup({
+    bold_keywords = true,
+    italic_comments = true,
+    -- FooBar
+    telescope = {
+      -- Available styles: `classic`, `flat`.
+      style = "classic",
+    },
+  })
+
+  vim.g.stroiman_nordic_setup = true
+end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyLoad",
+  group = "stroiman_plugin_load",
+  callback = function(ev)
+    if ev.data == "nordic" then
+      setup_nordic()
+    end
+  end,
+})
+
+if vim.g.stroiman_nordic_setup then
+  setup_nordic()
+end
